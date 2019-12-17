b82f1cee54a6d0f19394150dc23746d7c41fff7f
sylvanasGone <397009765@qq.com>
2019-12-17 17:10:33 +0800

    feat: add config-override config

---
+++ index.js
@@ -0,0 +1,39 @@
+const { paths } = require('react-app-rewired')
+const {
+  fixBabelImports,
+  addWebpackAlias,
+  override,
+  addBabelPlugins,
+  addPostcssPlugins,
+  useEslintRc
+} = require('customize-cra')
+const rewireHotLoader = require('react-app-rewire-hot-loader-for-customize-cra')
+const rewireCssModules = require('react-app-rewire-css-modules-extensionless')
+
+module.exports = override(
+  ...addBabelPlugins('babel-plugin-emotion'),
+  rewireCssModules(),
+  addPostcssPlugins([
+    require('postcss-preset-env')({
+      stage: 0
+    }),
+    require('postcss-flexbugs-fixes')
+  ]),
+  rewireHotLoader(),
+  fixBabelImports(
+    'import',
+    {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: 'css'
+    },
+    {
+      style: false,
+      libraryName: 'lodash',
+      libraryDirectory: null,
+      camel2DashComponentName: false
+    }
+  ),
+  useEslintRc('.eslintrc.json'),
+  addWebpackAlias({ '@': paths.appSrc })
+)
